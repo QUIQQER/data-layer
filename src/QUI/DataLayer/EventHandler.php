737cbbbcdad70506db6a09287f2a86b6c7102ec9
Henning Leutz <leutz@pcsg.de>
2023-12-07 10:13:59 +0100

    feat: event setting api

--- src/QUI/DataLayer/EventHandler.php
+++ src/QUI/DataLayer/EventHandler.php
@@ -12,14 +12,60 @@ use Quiqqer\Engine\Collector;
  */
 class EventHandler
 {
-    public static function onTemplateBegin(
+    public static function onTemplateHeaderBegin(
         Collector $Collection,
         QUI\Template $Template
     ) {
         $Collection->append('<script data-no-cache="1">window.dataLayer = window.dataLayer || [];</script>');
+
+        if ($Template->getAttribute('Project')) {
+            // insert header tags
+
+            /* @var $Project QUI\Projects\Project */
+            $DataLayer = new DataLayer();
+            $Project = $Template->getAttribute('Project');
+
+            $Collection->append(
+                $DataLayer->getHeaderBegin($Project)
+            );
+        }
     }
 
-    public static function onTemplateEnd(
+    public static function onTemplateHeaderEnd(
+        Collector $Collection,
+        QUI\Template $Template
+    ) {
+        if ($Template->getAttribute('Project')) {
+            // insert header tags
+
+            /* @var $Project QUI\Projects\Project */
+            $DataLayer = new DataLayer();
+            $Project = $Template->getAttribute('Project');
+
+            $Collection->append(
+                $DataLayer->getHeaderEnd($Project)
+            );
+        }
+    }
+
+    public static function onTemplateBodyBegin(
+        Collector $Collection,
+        QUI\Template $Template
+    ) {
+        if ($Template->getAttribute('Project')) {
+            // insert header tags
+
+            /* @var $Project QUI\Projects\Project */
+            $Project = $Template->getAttribute('Project');
+            $DataLayer = new DataLayer();
+
+            $Collection->append(
+                $DataLayer->getBodyBegin($Project)
+            );
+        }
+    }
+
+    public static function onTemplateBodyEnd(
         Collector $Collection,
         QUI\Template $Template
     ) {
@@ -28,5 +74,18 @@ class EventHandler
             '<script src="' . URL_OPT_DIR . 'quiqqer/data-layer/bin/trackingEcommerce.js"></script>' .
             '<script src="' . URL_OPT_DIR . 'quiqqer/data-layer/bin/trackingFrontendUsers.js"></script>'
         );
+
+        if ($Template->getAttribute('Project')) {
+            // insert header tags
+
+            /* @var $Project QUI\Projects\Project */
+            $DataLayer = new DataLayer();
+            $Project = $Template->getAttribute('Project');
+
+            $Collection->append(
+                $DataLayer->getBodyEnd($Project)
+            );
+        }
     }
+
 }
