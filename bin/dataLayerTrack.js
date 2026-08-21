16d9cc16b73bd66b855f1500019eb69b63b9bbe1
Henning <leutz@pcsg.de>
2026-08-21 15:50:36 +0200

    feat: track authentication lifecycle events
    Related: quiqqer/data-layer#2

--- bin/dataLayerTrack.js
+++ bin/dataLayerTrack.js
@@ -1,11 +1,31 @@
 window.whenQuiLoaded().then(function() {
     'use strict';
 
+    if (typeof window.qTrack !== 'function') {
+        return;
+    }
+
     window.qTrack('event', 'page_view', {
         'page_location': window.location.toString(),
         'page_title': document.title,
         'visitor_type': QUIQQER_USER.id ? 'user' : 'visitor',
         'site_type': QUIQQER_SITE.type.replace('quiqqer/', ''), // <- mor wollte das
         'site_id': QUIQQER_SITE.id
+    });
+
+    require(['qui/QUI'], function(QUI) {
+        QUI.addEvent('onQuiqqerUserAuthLoginSuccess', function(Instance, authenticator) {
+            const data = {};
+
+            if (typeof authenticator !== 'undefined' && authenticator !== '') {
+                data.method = authenticator;
+            }
+
+            window.qTrack('event', 'login', data);
+        });
+
+        QUI.addEvent('onQuiqqerFrontendUsersRegisterSuccess', function() {
+            window.qTrack('event', 'sign_up');
+        });
     });
 });
